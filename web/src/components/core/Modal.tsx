c661b91160cc1d34aaf9f90ed749ddd5f804e44b
Vivian Leung <vivz753@gmail.com>
2025-11-05 20:33:25 -0800

    refactor scrollToElement to helpers folder, Modal: make buttons transparent

--- web/src/components/core/Modal.tsx
+++ web/src/components/core/Modal.tsx
@@ -1,22 +1,26 @@
-import { forwardRef } from "react"
+import { useState, forwardRef, useEffect } from "react"
 import clsx from "clsx"
 import Image from "next/image"
 import { Art } from "@schemas/global"
 import { ChevronLeftIcon, ChevronRightIcon, XMarkIcon } from "@heroicons/react/24/solid"
+import { scrollToElement } from "@helpers/index"
 
 interface ModalProps {
   show: boolean
   onClose: () => void
   onPrev: () => void
   onNext: () => void
-  image: Art
+  project: Art
   children?: React.JSX.Element
 }
 
-const Modal = forwardRef<HTMLDivElement, ModalProps>(({ show, onClose, onPrev, onNext, children, image }, ref) => {
+const contentId = "content"
+
+const Modal = forwardRef<HTMLDivElement, ModalProps>(({ show, onClose, onPrev, onNext, children, project }, ref) => {
   return (
     <div className={clsx("fixed top-0 z-100 h-full w-screen justify-center", show ? "flex" : "hidden")}>
       <div className="relative flex h-full w-full items-center justify-center">
+        {/* TODO: fix the outer click to exit function */}
         <div
           onClick={() => {
             console.log("clicked")
@@ -24,38 +28,63 @@ const Modal = forwardRef<HTMLDivElement, ModalProps>(({ show, onClose, onPrev, o
           }}
           className="absolute h-full w-full bg-black opacity-70"
         />
-        {/* <div  ref={ref} className="absolute bg-white rounded-xl inset-14 m-10 bg-contain bg-no-repeat bg-center"  style={{backgroundImage: `url(${activeImage})`}}>
+        {/* <div  ref={ref} className="absolute bg-white rounded-xl inset-14 m-10 bg-contain bg-no-repeat bg-center"  style={{backgroundImage: `url(${activeproject})`}}>
         {children}
       </div> */}
         <div className="flex h-full w-full flex-row items-center justify-center">
-          <div className="relative flex h-full w-full items-center justify-center p-4 md:p-8 lg:p-8">
-            <div className="relative flex h-full w-full rounded-lg bg-white">
-              <Image alt={image.title} fill style={{ objectFit: "contain" }} src={image.url} />
-            </div>
-            <button
-              onClick={() => onPrev()}
-              className="absolute left-2 lg:left-8 m-4 flex shrink-0 items-center justify-center rounded-full bg-yellow-600 p-2"
-            >
-              <span className="w-full cursor-pointer text-white">
-                <ChevronLeftIcon className="size-5 lg:size-7 text-white" />
-              </span>
-            </button>
+          {/* Fixed top bar */}
+          <div className="fixed top-0 left-0 z-[1] flex w-full items-center">
+            {/* Title */}
+            {project.title && <p className="w-full bg-black/35 p-8 text-center text-white lg:p-4">{project.title}</p>}
+            {/* X Close */}
             <button
               onClick={onClose}
-              className="absolute top-8 lg:top-12 flex min-w-max shrink-0 items-center justify-center rounded-full bg-yellow-600 p-2"
+              className="absolute left-2 flex min-w-max shrink-0 items-center justify-center p-1 lg:left-12"
             >
-              <span className="w-full cursor-pointer text-white">
-                <XMarkIcon className="size-5 lg:size-7 text-white" />
+              <span className="flex w-full cursor-pointer flex-row items-center text-white">
+                <XMarkIcon className="mt-1 size-7 text-white" />
+                <span className="hidden underline lg:flex">close</span>
               </span>
             </button>
-            <button
-              onClick={() => onNext()}
-              className="absolute right-2 lg:right-8 m-4 flex min-w-max shrink-0 items-center justify-center rounded-full bg-yellow-600 p-2"
-            >
-              <span className="w-full cursor-pointer text-white">
-                <ChevronRightIcon className="size-5 lg:size-7 text-white" />
-              </span>
-            </button>
+          </div>
+          <div className="relative flex h-full w-full items-center justify-center lg:mr-4">
+            <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-md">
+              <div className="relative inline-block h-full w-full flex-row overflow-hidden overflow-y-auto bg-black">
+                <div className="relative flex h-full w-full overflow-hidden rounded-md">
+                  <Image alt={project.title} fill style={{ objectFit: "contain" }} src={project.url} />
+                </div>
+                {/* View More */}
+                {project.content && (
+                  <button
+                    onClick={() => scrollToElement(contentId)}
+                    className="absolute bottom-0 z-[1] flex items-center"
+                  >
+                    <p className="w-full bg-white/15 p-4 text-center text-sm text-white underline">View More</p>
+                  </button>
+                )}
+                {project.content && (
+                  <div id={contentId} className="flex h-screen items-center justify-center">
+                    {project.content}
+                  </div>
+                )}
+              </div>
+              <button
+                onClick={() => onPrev()}
+                className="absolute left-1 flex shrink-0 items-center justify-center bg-black/25 p-2 lg:left-8 lg:bg-white/15"
+              >
+                <span className="w-full cursor-pointer text-white">
+                  <ChevronLeftIcon className="size-5 text-white lg:size-7" />
+                </span>
+              </button>
+              <button
+                onClick={() => onNext()}
+                className="absolute right-1 flex min-w-max shrink-0 items-center justify-center bg-black/25 p-2 lg:right-8 lg:bg-white/15"
+              >
+                <span className="w-full cursor-pointer text-white">
+                  <ChevronRightIcon className="size-5 text-white lg:size-7" />
+                </span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
