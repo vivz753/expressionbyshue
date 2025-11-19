42bb1daf219931ac952b23f6bf62204663e1bcf6
Vivian Leung <vivz753@gmail.com>
2025-11-18 21:07:42 -0800

    update sanity

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,12 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'lpnrji1s',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
+  deployment: {autoUpdates: true},
   studioHost: 'expressionbyshue',
 })
